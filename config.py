2dc2591c7da44b4e7ca9a4daabebd0fe6a065385
Gart2357 <gart@outlook.jp>
2022-06-15 15:47:24 +0900

    １期生を追加

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 my_playlist_id = "PL0Dc_rBvxTTWg1LYKt0Aq6EtnXkRyuL8H"
 
 target_playlist_ids = [
+    # hololive IDOL PROJECT
+    "PL1NeGg1woXqkTks79_kSoxBrEKloB9SrI",
     # ときのそら
     "PL_0A0t0-Y0AMGFPCuKDZ3o8PMVpKcNvOQ",
     # ロボ子さん
@@ -11,8 +13,20 @@ target_playlist_ids = [
     "PLAo9RlHR2tDZwddeEyp9nTfpaFB58DrXd", # オリジナル曲
     "PLAo9RlHR2tDZRNIVsqxS3U8kTgyROY1Xg", # 参加楽曲
     "PLAo9RlHR2tDYvFj5mxCkNHXCw4qqpQG7Z", # Midnight Grand Orchestra
-    "PLAo9RlHR2tDakgakxbOxT9dAZD0rpwvXQ", # 歌ってみた
+    "PLAo9RlHR2tDakgakxbOxT9dAZD0rpwvXQ", # カバー
     # AZKi
     "PLpt61bADOMwW2aA9I1hWHyR8OixSuCXzc", # オリジナル曲
-    "PLpt61bADOMwXIZpLr09sCNeocN7CXjcoS", # 歌ってみた
+    "PLpt61bADOMwXIZpLr09sCNeocN7CXjcoS", # カバー
+    # 夜空メル
+    "PL43udV6iXTkCwnaFSjm3IAtggd7f12onJ", # カバー
+    # アキ・ローゼンタール
+    "PLYt9gyZFZDj_e7p0saJXMghPsjmBMwyiO",
+    # 赤井はあと
+    "PLQoA24ikdy_nAOv_I7586LS891s5es8AC", # オリジナル曲
+    "PLQoA24ikdy_kFurrcDRRJufTwHo_Kv6ew", # カバー
+    # 白上フブキ
+    "PL7LdRPp7xCkP9JJYvAqrQJLScbSol7S4-", # ソロ
+    "PL7LdRPp7xCkOwyOEKkE5YNC1_pGCTU2Id", # コラボ
+    # 夏色まつり
+    "PL6sZ3uYmeG1vGZVLNlWdJkm3W0R86y_Qa",
 ]
